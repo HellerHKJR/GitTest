b139a1e1667e2bae1a8a0aafe42e7a768901ec10
jryou <jryu@hellerindustries.co.kr>
2018-08-13 16:10:31 +0900

    Revert "Test Gio edit at bransh"
    This reverts commit 0da22a58adbdbd590d7f3936fd389d55bdfef39a.

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -21,8 +21,6 @@ namespace HelloWorld
             MessageBox.Show("Test Git");
 
             MessageBox.Show("Test Git Again");
-
-            MessageBox.Show("Test Git Edit at Branch!");
         }
     }
 }
